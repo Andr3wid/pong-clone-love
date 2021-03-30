ade3f20feb54267adc7b9589185f97add7ad1289
Andreas Kogler <andy.kogler.junior@gmail.com>
2021-03-30 14:25:52 +0200

    Introduce title-screen

--- main.lua
+++ main.lua
@@ -9,6 +9,7 @@ P2_LABEL = "Player 2:\n"
 BAR_LENGTH = 50         -- 'width' of the rectangle that represents a players bar
 BAR_THICKNESS = 12      -- 'height' of the rectangle that represents a players bar
 BAR_MOVE_SPEED = 300
+VICTORY_THRESHOLD = 3
 
 -- variable definitions for player 1 rectangle
 p1_score = 0
@@ -26,38 +27,52 @@ BALL_WIDTH  = 10
 BALL_SPEEDUP = 1.1 -- 10% speedup on each collision with a bar
 ball_x = WINDOW_WIDTH/2 - BALL_WIDTH/2
 ball_y = WINDOW_HEIGHT/2 - BALL_HEIGHT/2
-ball_dx = 0
-ball_dy = 0
+ball_dx, ball_dy = 0
+
+game_state = "titlescreen"
 
 -- called one time upon start of the game
 function love.load()
     love.window.setTitle(GAME_TITLE)
     love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {})
 
-    spawnBall()
+    initGame()
 end
 
 -- called on each game-loop iteration passing deltatime
 function love.update(dt)
-    if ball_x <= 0 then
-        p2_score = p2_score + 1
-        spawnBall()
+
+    if game_state == "playing" then
+        if ball_x <= 0 then
+            p2_score = p2_score + 1
+            spawnBall()
+        end
+
+        if ball_x >= WINDOW_WIDTH then
+            p1_score = p1_score + 1
+            spawnBall()
+        end
+        ball_x = ball_x + ball_dx*dt
+        ball_y = ball_y + ball_dy*dt
+
+        detectBarMovement(dt)
+        handleBarCollision()
+        handleScreenEdgeCollision()
     end
 
-    if ball_x >= WINDOW_WIDTH then
-        p1_score = p1_score + 1
-        spawnBall()
-    end
-    ball_x = ball_x + ball_dx*dt
-    ball_y = ball_y + ball_dy*dt
-
-    detectBarMovement(dt)
-    handleBarCollision()
-    handleScreenEdgeCollision()
 end
 
 -- called on each game-loop iteration after love.update to render graphics
 function love.draw() 
+
+    -- display menu when the game is started
+    if game_state == "titlescreen" then
+        love.graphics.printf("Press <SPACE> to start", 0, WINDOW_HEIGHT/2 - 100, WINDOW_WIDTH, 'center')
+        love.graphics.printf("Player 1: up <w>, down <s>", 0, WINDOW_HEIGHT/2 - 60, WINDOW_WIDTH, 'center')
+        love.graphics.printf("Player 2: up <i>, down <k>", 0, WINDOW_HEIGHT/2 - 80, WINDOW_WIDTH, 'center')
+
+    end
+
     -- game title & labels
     love.graphics.printf(GAME_TITLE, 0, 10, WINDOW_WIDTH, 'center')
     love.graphics.printf(P1_LABEL..p1_score, 0, 10, WINDOW_WIDTH, 'left', 0, 1, 1, -20)
@@ -71,6 +86,14 @@ function love.draw()
 
     -- ball
     love.graphics.rectangle('fill', ball_x, ball_y, BALL_WIDTH, BALL_HEIGHT)
+end
+
+-- handle keypresses in menus
+function love.keypressed(key)
+    if game_state == "titlescreen" and key == "space" then
+        spawnBall()
+        game_state = "playing"
+    end
 end
 
 -- handle keyboard input
@@ -139,4 +162,17 @@ function handleScreenEdgeCollision()
     if ball_y <= 0 or ball_y + BALL_HEIGHT >= WINDOW_HEIGHT then
         ball_dy = -ball_dy
     end
+end
+
+-- starts a new game by resetting all values to default
+function initGame() 
+    -- variable definitions for player 1 rectangle
+    p1_score = 0
+    p1_bar_x = 10
+    p1_bar_y = WINDOW_HEIGHT/2 - BAR_LENGTH/2
+
+    -- variable definitions for player 2 rectangle
+    p2_score = 0
+    p2_bar_x = WINDOW_WIDTH - BAR_THICKNESS - 10
+    p2_bar_y = WINDOW_HEIGHT/2 - BAR_LENGTH/2
 end
